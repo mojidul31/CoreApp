b66ee50b473372d777d0fe7ce2299f003601addc
Mojidul Islam <mojidul31@gmail.com>
2022-10-29 17:01:15 +0600

    Add project files.

---
+++ CoreApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CoreApp.com.test;
+
+Console.WriteLine("Hello, World!");
+
+var str = " Hello   Mojidul   Islam      ";
+Console.WriteLine(ProblemAndSolutionOne.RemoveSpaceFromStr(str));
---
+++ CoreApp/com.test/ProblemAndSolutionOne.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreApp.com.test
+{
+    public class ProblemAndSolutionOne
+    {
+		public static string RemoveSpaceFromStr(string str)
+		{
+			StringBuilder sb = new StringBuilder();
+			str = str.Trim();
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (Char.IsWhiteSpace(str[i]))
+				{
+					if (!Char.IsWhiteSpace(str[i - 1]))
+					{
+						sb.Append(str[i]);
+					}
+				}
+				else
+				{
+					sb.Append(str[i]);
+				}
+			}
+			return sb.ToString();
+		}
+	}
+}
