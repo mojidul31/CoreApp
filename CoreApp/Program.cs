9db7a8d0db21d6f5c9bdfcefc3f11c913d4a7c8a
Mojidul Islam <mojidul.dev.pwd@gmail.com>
2022-11-11 22:04:26 +0600

    add

--- CoreApp/Program.cs
+++ CoreApp/Program.cs
@@ -5,3 +5,20 @@ Console.WriteLine("Hello, World!");
 
 var str = " Hello   Mojidul   Islam      ";
 Console.WriteLine(ProblemAndSolutionOne.RemoveSpaceFromStr(str));
+//remove duplicate value from an int array
+int[] nums = { 1, 1, 2 };
+Console.WriteLine(ProblemAndSolutionOne.RemoveDuplicates(nums));
+var withoutDuplicateValues = ProblemAndSolutionOne.RemoveDuplicates2(nums);
+//Array.ForEach(withoutDuplicateValues, x=> Console.WriteLine("[{0}]",',', x));
+Console.WriteLine("[{0}]", string.Join(',', withoutDuplicateValues));
+//How to combine two arrays without duplicate values in C#?
+string[] arr1 = { "Cat", "Alligator", "Fox", "Donkey", "Cat" };
+string[] arr2 = { "Sparrow", "Peacock", "Dove", "Crow" };
+string[] combineArray = ProblemAndSolutionOne.combineTwoArrays(arr1, arr2);
+Console.WriteLine("[{0}]",string.Join(',',combineArray));
+//How to get a comma-separated string from the integer array
+int[] nums1 = { 1, 2, 3, 4 };
+//Console.WriteLine("[{0}]", string.Join(',', nums1));
+Console.WriteLine("{0}", string.Join(',', nums1));
+
+
--- CoreApp/com.test/ProblemAndSolutionOne.cs
+++ CoreApp/com.test/ProblemAndSolutionOne.cs
@@ -29,5 +29,24 @@ namespace CoreApp.com.test
 			}
 			return sb.ToString();
 		}
+
+		//How to remove unwanted space from a string
+		public static int RemoveDuplicates(int[] nums)
+		{
+			return nums.Distinct().Count();
+		}
+
+		public static int[] RemoveDuplicates2(int[] nums)
+		{
+			return nums.Distinct().ToArray();
+		}
+
+		//How to combine two arrays without duplicate values in C#?
+		public static string[] combineTwoArrays(string[] arr1, string[] arr2)
+        {
+			return arr1.Union(arr2).ToArray();
+		}
+		
+
 	}
 }
