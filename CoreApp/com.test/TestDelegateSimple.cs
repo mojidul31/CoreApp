c6e1e1370bcd01bc56af585868897a9b7d3d198f
Mojidul Islam <mojidul31@gmail.com>
2025-04-18 10:05:30 +0600

    add-delegate

---
+++ CoreApp/com.test/TestDelegateSimple.cs
@@ -0,0 +1,28 @@
+﻿namespace CoreApp.com.test
+{
+    public class TestDelegateSimple
+    {        
+        public void sum(int a, int b)
+        {
+            Console.WriteLine("(100 + 40) = {0}", a + b);
+        }
+
+        public void subtract(int a, int b)
+        {
+            Console.WriteLine("(100 - 60) = {0}", a - b);
+        }
+
+        // "area" method
+        public void area(double height, double width)
+        {
+            Console.WriteLine("Area is: {0}", (width * height));
+        }
+
+        // "perimeter" method
+        public void perimeter(double height, double width)
+        {
+            Console.WriteLine("Perimeter is: {0} ", 2 * (width + height));
+        }
+
+    }
+}
